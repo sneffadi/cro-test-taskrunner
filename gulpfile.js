3c72c8d51a36a75d04efb18b4afc8d7dc2c3790e
Stephanie Jelalian <sneffadi@gmail.com>
2020-01-30 14:45:10 -0700

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,31 @@
+'use strict'
+
 var gulp = require("gulp");
 const sass = require('gulp-sass');
 var babel = require("gulp-babel");
 var watch = require('gulp-watch');
+var gutil = require('gulp-util');
+var ftp = require('vinyl-ftp');
+
+/** FTP Configuration **/
+var user = process.env.FTP_USER
+var password = process.env.FTP_PWD
+var host = ''
+var port = 21
+var localFilesGlob = ['./**']
+var remoteFolder = '' + process.env.TEST_PATH
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {
+  return ftp.create({
+    host: host,
+    port: port,
+    user: user,
+    password: password,
+    parallel: 5,
+    log: gutil.log,
+  })
+}
 
 gulp.task('styles', () => {
     return gulp.src('*.scss')
@@ -15,6 +39,7 @@ gulp.task('clean', () => {
     ]);
 });
 
+/* compresses scss file in whatever folder you're in on the terminal */
 gulp.task('sass-single', function () {
       process.chdir(process.env.INIT_CWD);
 
@@ -39,9 +64,28 @@ gulp.task("babels", function () {
     .pipe(gulp.dest("dist"));
 });
 
+/* watches for scss changes */
 gulp.task('watch', function(){
   process.chdir(process.env.INIT_CWD);
   gulp.watch('*.scss', gulp.task('sass-single'));
 });
 
+/**
+ * Deploy task.
+ * Copies the new files to the server
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd TEST_PATH=filepath gulp ftp-deploy`
+ */
+gulp.task('ftp-deploy', function() {
+  process.chdir(process.env.INIT_CWD);
+  var conn = getFtpConnection()
+
+  return gulp
+    .src(localFilesGlob, { base: '.', buffer: false })
+    .pipe(conn.newer(remoteFolder)) // only upload newer files
+    .pipe(conn.dest(remoteFolder))
+});
+
+/*  */
 gulp.task('default', gulp.series('sass-single', 'babels'));
+
